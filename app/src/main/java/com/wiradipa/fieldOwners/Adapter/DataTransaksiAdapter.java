c8fd28b8599a7c5ee3cf7e0231c9d9a26329cb64
Bambang Stiawan <b3ng.b3ng16@gmail.com>
2018-11-11 04:32:03 +0700

    Lanjut Data Transaksi

--- app/src/main/java/com/wiradipa/fieldOwners/Adapter/DataTransaksiAdapter.java
+++ app/src/main/java/com/wiradipa/fieldOwners/Adapter/DataTransaksiAdapter.java
@@ -1,23 +1,42 @@
 package com.wiradipa.fieldOwners.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.gson.Gson;
+import com.wiradipa.fieldOwners.ApiHelper.BaseApiService;
+import com.wiradipa.fieldOwners.ApiHelper.UtilsApi;
 import com.wiradipa.fieldOwners.Model.DataTransaksi;
-import com.wiradipa.fieldOwners.Model.Jadwal;
+import com.wiradipa.fieldOwners.Model.FieldTariff;
 import com.wiradipa.fieldOwners.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdapter.ViewHolder> {
 
@@ -33,6 +52,7 @@ public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdap
         TextView tvWaktuMain;
         TextView tvNamaLapangMain;
         TextView tvTotalTagihan;
+        TextView tvStatusApprove;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -43,6 +63,7 @@ public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdap
             tvWaktuMain = (TextView) itemView.findViewById(R.id.tr_waktu_main);
             tvNamaLapangMain = (TextView) itemView.findViewById(R.id.tr_lapang_main);
             tvTotalTagihan = (TextView) itemView.findViewById(R.id.tr_total_tagihan);
+            tvStatusApprove = (TextView) itemView.findViewById(R.id.tr_status_approve);
         }
     }
 
@@ -68,13 +89,68 @@ public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdap
             ((ViewHolder) viewHolder).tvNamaLapangPenyewa.setText((dataTransaksi.mNamaLapangPenyewa));
             ((ViewHolder) viewHolder).tvWaktuMain.setText((String.valueOf(dataTransaksi.mWaktuMain + " Jam")));
             ((ViewHolder) viewHolder).tvNamaLapangMain.setText((dataTransaksi.mNamaLapangMain));
-            ((ViewHolder) viewHolder).tvTotalTagihan.setText((dataTransaksi.mTotalTagihan));
+            ((ViewHolder) viewHolder).tvTotalTagihan.setText((checkDigitMoney(dataTransaksi.mTotalTagihan)));
+            if (dataTransaksi.mStatus == 0){
+                ((ViewHolder) viewHolder).tvStatusApprove.setText("Belum disetujui");
+                ((ViewHolder) viewHolder).tvStatusApprove.setTextColor(mContext.getResources().getColor(R.color.not_approved));
+            } else {
+                ((ViewHolder) viewHolder).tvStatusApprove.setText("Disetujui");
+                ((ViewHolder) viewHolder).tvStatusApprove.setTextColor(mContext.getResources().getColor(R.color.approved));
+            }
         }
     }
 
     @Override
     public int getItemCount() {
         return listTransaksi.size();
+    }
+
+    public String checkDigitMoney(int number) {
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+        return formatRupiah.format((double) number);
+    }
+
+
+
+    public void dialogReceiptUrl(String url){
+        String titleText = "Tambah Tarif";
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        View dialogView = LayoutInflater.from(mContext).inflate(R.layout.custom_alert_dialog, null);
+        ImageView imageView = (ImageView) dialogView.findViewById(R.id.imageReceipt);
+        Glide.with(mContext).load("http://app.lapangbola.com" + url).into(imageView);
+        dialogBuilder.setView(imageView);
+        dialogBuilder.setTitle(titleText);
+        dialogBuilder.setCancelable(true);
+//        // Initialize a new foreground color span instance
+//        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(Color.WHITE);
+//        // Initialize a new spannable string builder instance
+//        SpannableStringBuilder ssBuilder = new SpannableStringBuilder(titleText);
+//        // Apply the text color span
+//        ssBuilder.setSpan(
+//                foregroundColorSpan,
+//                0,
+//                titleText.length(),
+//                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//        );
+//        dialogBuilder.setTitle(ssBuilder);
+
+        dialogBuilder.setPositiveButton("Tambah", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialogBuilder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000046")));
+        dialog.show();
     }
 
     public void parsingDataJson(JSONArray dataJsonArray){
@@ -92,7 +168,9 @@ public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdap
                 jsonTransaksi.mEndHour = jsonObject.getInt("end_hour");
                 jsonTransaksi.mWaktuMain = jsonTransaksi.mEndHour - jsonTransaksi.mStartHour;
                 jsonTransaksi.mNamaLapangMain = jsonObject.getString("field_name");
-                jsonTransaksi.mTotalTagihan = jsonObject.getString("amount");
+                jsonTransaksi.mTotalTagihan = jsonObject.getInt("amount");
+                jsonTransaksi.urlBuktiDp = jsonObject.getString("receipt_url");
+                jsonTransaksi.mStatus = jsonObject.getInt("down_payment_status");
                 listTransaksi.add(jsonTransaksi);
             }
         } catch (JSONException e) {
@@ -102,5 +180,9 @@ public class DataTransaksiAdapter extends RecyclerView.Adapter<DataTransaksiAdap
 
     public int getTransaksiId(int position){
         return listTransaksi.get(position).id;
+    }
+
+    public String getReceiptUrl(int position){
+        return listTransaksi.get(position).urlBuktiDp;
     }
 }
--- app/src/main/java/com/wiradipa/fieldOwners/DetailTransaksiActivity.java
+++ app/src/main/java/com/wiradipa/fieldOwners/DetailTransaksiActivity.java
@@ -3,14 +3,23 @@ package com.wiradipa.fieldOwners;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.wiradipa.fieldOwners.ApiHelper.AppSession;
 import com.wiradipa.fieldOwners.ApiHelper.BaseApiService;
 import com.wiradipa.fieldOwners.ApiHelper.UtilsApi;
@@ -19,6 +28,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import okhttp3.ResponseBody;
@@ -33,13 +44,13 @@ public class DetailTransaksiActivity extends AppCompatActivity {
     AppSession mAppSession;
 
     TextView tvPlayerName, tvFieldName, tvVenueName, tvDate, tvIdTransaction
-            , tvTime, tvClubPlayer;
-    Button btnLunas, btnBatal;
+            , tvTime, tvClubPlayer, tvDownPayment, tvReceiveable, tvAmount;
+    Button btnLunas, btnBatal, btnApprove, btnProofPayment;
     CircleImageView civPlayer;
 
-    String mIdTransaksi, mNamaLapang, mNamaVenue, mTagihan, mStartHour, mEndHour,
-            mTanggal, mPayment, mPenyewa;
-    int id;
+    String mIdTransaksi, mNamaLapang, mNamaVenue, mStartHour, mEndHour,
+            mTanggal, mPayment, mPenyewa, mUrlBukti;
+    int id, mUangMuka, mPiutang, mTagihan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +64,10 @@ public class DetailTransaksiActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras!=null){
             id = extras.getInt("idTransaction");
+            mUrlBukti = extras.getString("receiptUrl");
             Toast.makeText(mContext, id + "", Toast.LENGTH_SHORT).show();
         }
+
         initComponents();
         detailTransaction();
     }
@@ -67,16 +80,21 @@ public class DetailTransaksiActivity extends AppCompatActivity {
         tvDate = (TextView) findViewById(R.id.detail_tr_tanggalMain);
         tvTime = (TextView) findViewById(R.id.detail_tr_time);
         tvClubPlayer = (TextView) findViewById(R.id.club_player);
+        tvDownPayment = (TextView) findViewById(R.id.detail_down_payment);
+        tvReceiveable = (TextView) findViewById(R.id.detail_receiveable);
+        tvAmount = (TextView) findViewById(R.id.detail_amount);
 
         civPlayer = (CircleImageView) findViewById(R.id.photo_player);
 
         btnLunas = (Button) findViewById(R.id.btnLunas);
         btnBatal = (Button) findViewById(R.id.btnBatal);
+//        btnApprove = (Button) findViewById(R.id.btnApproveDP);
+        btnProofPayment = (Button) findViewById(R.id.proofOfPayment);
 
         btnLunas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
@@ -86,10 +104,41 @@ public class DetailTransaksiActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+//        btnApprove.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                approveDownPayment();
+//            }
+//        });
+
+        btnProofPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogReceiptUrl(mUrlBukti);
+            }
+        });
     }
 
     public String checkDigit(String number) {
         return Integer.parseInt(number)<=9?"0"+number+".00":number +".00";
+    }
+
+    public String checkDigitMoney(int number) {
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+        return formatRupiah.format((double) number);
+    }
+
+    public void popupAllert(String alert) {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_title_error)
+                .setMessage(alert)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int arg1) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
     }
 
     private void detailTransaction(){
@@ -100,41 +149,161 @@ public class DetailTransaksiActivity extends AppCompatActivity {
 
         mApiService.detailTransaksi(id, mAppSession.getData(AppSession.TOKEN), 1)
                 .enqueue(new Callback<ResponseBody>() {
-                    @SuppressLint("SetTextI18n")
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        if (response.isSuccessful()){
-                            progressDialog.dismiss();
-                            try {
-                                JSONObject data = new JSONObject(response.body().string());
-                                if (data.getString("status").equals("Success")){
-                                    mIdTransaksi = data.getString("id_transaction");
-                                    mPenyewa = data.getString("player_name");
-                                    mTanggal = data.getString("rental_date");
-                                    mStartHour = data.getString("start_hour");
-                                    mEndHour = data.getString("end_hour");
-                                    mNamaLapang = data.getString("field_name");
-                                    mNamaVenue = data.getString("field_owner_name");
-                                    mTagihan = data.getString("amount");
-                                    mPayment = data.getString("payment_status");
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()){
+                    progressDialog.dismiss();
+                    try {
+                        JSONObject data = new JSONObject(response.body().string());
+                        if (data.getString("status").equals("Success")){
+                            mIdTransaksi = data.getString("id_transaction");
+                            mPenyewa = data.getString("player_name");
+                            mTanggal = data.getString("rental_date");
+                            mStartHour = data.getString("start_hour");
+                            mEndHour = data.getString("end_hour");
+                            mNamaLapang = data.getString("field_name");
+                            mNamaVenue = data.getString("field_owner_name");
+                            mPayment = data.getString("payment_status");
+                            mTagihan = data.getInt("amount");
+//                            mUangMuka = data.getInt("down_payment");
+//                            mPiutang = data.getInt("receivable");
 
-                                    tvPlayerName.setText(mPenyewa);
-                                    tvFieldName.setText(mNamaLapang);
-                                    tvVenueName.setText(mNamaVenue);
-                                    tvDate.setText(mTanggal);
-                                    tvIdTransaction.setText(mIdTransaksi);
-                                    tvTime.setText(checkDigit(mStartHour) + " - " + checkDigit(mEndHour));
-                                }
-                            } catch (IOException | JSONException e) {
-                                e.printStackTrace();
-                            }
+                            tvPlayerName.setText(mPenyewa);
+                            tvFieldName.setText(mNamaLapang);
+                            tvVenueName.setText(mNamaVenue);
+                            tvDate.setText(mTanggal);
+                            tvIdTransaction.setText(mIdTransaksi);
+                            tvTime.setText(checkDigit(mStartHour) + " - " + checkDigit(mEndHour));
+//                            tvDownPayment.setText(checkDigitMoney(mUangMuka));
+//                            tvReceiveable.setText(checkDigitMoney(mPiutang));
+                            tvAmount.setText(checkDigitMoney(mTagihan));
+                        } else {
+                            String errMsg = data.getString("message");
+                            popupAllert(errMsg);
                         }
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
                     }
+                } else {
+                    switch (response.code()){
+                        case 404:
+                            popupAllert(getString(R.string.server_not_found));
+                            break;
+                        case 500:
+                            popupAllert(getString(R.string.server_error));
+                            break;
+                        default:
+                            popupAllert(getString(R.string.unknown_error));
+                    }
+                }
+            }
 
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("debug", "OnFailure: ERROR > "+ t.toString());
+                progressDialog.dismiss();
+                popupAllert("No Internet Connection !!!");
+            }
+        });
+    }
+
+    private void approveDownPayment(){
+        final ProgressDialog progressDialog = new ProgressDialog(mContext);
+        progressDialog.setTitle("Proses");
+        progressDialog.setMessage("Tunggu Sebentar");
+        progressDialog.show();
+
+        mApiService.approveDownPayment(id, mAppSession.getData(AppSession.TOKEN))
+                .enqueue(new Callback<ResponseBody>() {
                     @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()){
+                    progressDialog.dismiss();
+                    try {
+                        JSONObject data = new JSONObject(response.body().string());
+                        String message = data.getString("message");
+                        if (data.getString("status").equals("Success")){
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            popupAllert(message);
+                        }
+                    } catch (JSONException | IOException e) {
+                        e.printStackTrace();
                     }
-                });
+                } else {
+                    switch (response.code()){
+                        case 404:
+                            popupAllert(getString(R.string.server_not_found));
+                            break;
+                        case 500:
+                            popupAllert(getString(R.string.server_error));
+                            break;
+                        default:
+                            popupAllert(getString(R.string.unknown_error));
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("debug", "OnFailure: ERROR > "+ t.toString());
+                progressDialog.dismiss();
+                popupAllert("No Internet Connection !!!");
+            }
+        });
+    }
+
+    public void dialogReceiptUrl(String url){
+        String titleText = "Tambah Tarif";
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.custom_alert_dialog, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageReceipt);
+        TextView msgNotAvailable = (TextView) view.findViewById(R.id.img_not_available);
+        if (url.equals("") || url == null || url.equals("/receipts/original/missing.png")){
+            msgNotAvailable.setVisibility(View.VISIBLE);
+            msgNotAvailable.setText(R.string.image_not_available);
+            imageView.setImageResource(R.drawable.ic_image_black_24dp);
+        } else {
+            msgNotAvailable.setVisibility(View.GONE);
+            Glide.with(mContext).load("http://app.lapangbola.com" + url).apply(new RequestOptions().placeholder(R.drawable
+                    .ic_image_black_24dp).error(R.drawable.ic_image_black_24dp)).into(imageView);
+        }
+        dialogBuilder.setView(view);
+        dialogBuilder.setTitle(titleText);
+
+        dialogBuilder.setPositiveButton("Setuju", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                approveDownPayment();
+            }
+        });
+        dialogBuilder.setNeutralButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(mContext, "Dalam Tahap Pengembangan", Toast.LENGTH_SHORT).show();
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialogBuilder.setNegativeButton("Tolak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        final AlertDialog dialog = dialogBuilder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.not_approved));
+                dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.approved));
+                dialog.getButton(DialogInterface.BUTTON_NEUTRAL).setTextColor(Color.parseColor("#000000"));
+
+            }
+        });
+        dialog.show();
     }
 }
